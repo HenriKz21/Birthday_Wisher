fffdd443decb60377e5d17c84a0c643994ed38de
HenriKz21 <kyloh881@gmail.com>
2026-08-22 23:10:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if today_tuple in birthdays_dict:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("pydiscover570@gmail.com") as connection:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
